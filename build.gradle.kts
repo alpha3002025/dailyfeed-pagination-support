4ae653e29a61af4e724030c292a3fecde58d0767
alpha3002025 <alpha300uk@gmail.com>
2025-09-12 15:15:48 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.5"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "click.dailyfeed"
+version = "0.0.1-SNAPSHOT"
+description = "😃😃😃"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.data:spring-data-commons")
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}

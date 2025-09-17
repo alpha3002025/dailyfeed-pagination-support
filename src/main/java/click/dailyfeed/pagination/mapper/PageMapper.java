018c329d54bc824dfa1b4c230f16b7e96e4ccc2d
alpha3002025 <alpha300uk@gmail.com>
2025-09-17 10:21:15 +0900

    feat: DailyfeedPage 변환로직 추가

--- src/main/java/click/dailyfeed/pagination/mapper/PageMapper.java
+++ src/main/java/click/dailyfeed/pagination/mapper/PageMapper.java
@@ -1,5 +1,6 @@
 package click.dailyfeed.pagination.mapper;
 
+import click.dailyfeed.code.global.web.response.DailyfeedPage;
 import click.dailyfeed.code.global.web.response.DailyfeedScrollPage;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
@@ -8,11 +9,25 @@ import java.util.List;
 
 @Component
 public class PageMapper {
-    public <D, T> DailyfeedScrollPage<T> fromJpaPage(Page<D> page, List<T> result) {
+    public <D, T> DailyfeedScrollPage<T> fromJpaPageToScrollPage(Page<D> page, List<T> result) {
         return DailyfeedScrollPage.<T>builder()
                 .content(result)
                 .page(page.getNumber())
                 .size(page.getSize())
+                .build();
+    }
+
+    public <D, T>DailyfeedPage<T> fromJpaPageToDailyfeedPage(Page<D> page, List<T> content) {
+        return DailyfeedPage.<T>builder()
+                .content(content)
+                .page(page.getNumber())
+                .size(page.getSize())
+                .totalElements(page.getTotalElements())
+                .totalPages(page.getTotalPages())
+                .isFirst(page.isFirst())
+                .isLast(page.isLast())
+                .hasNext(page.hasNext())
+                .hasPrevious(page.hasPrevious())
                 .build();
     }
 }
